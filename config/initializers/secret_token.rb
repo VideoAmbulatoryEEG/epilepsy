864cbbfc93ac5db4b9372a891c2f2e41e7ed0087
sulabh jain <sulabh.jain@case.edu>
2011-09-13 15:24:49 -0400

    Project directories created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Epilepsy::Application.config.secret_token = 'e76ece1ff52d6b0f843700873e9d77400fce36850f9113ea2d89e016dc458dd7f06ff69ab5643e4462ac2f1f0b54dbd483ee4afaa1ad10b92c4c893692db1a00'
